5daf96f8e59553427251e4d87b49652df356dee1
khalifa <mnpel789f@gmail.com>
2021-09-12 15:39:51 +0200

    finish app

--- lib/Models/CallsModel.dart
+++
@@ -1,9 +0,0 @@
-class CallsModel
-{
-  String name;
-  String time;
-  String icon;
-  bool icall;
-
-  CallsModel({this.name,this.icon,this.time,this.icall});
-}
--- lib/Models/statusModel.dart
+++
@@ -1,9 +0,0 @@
-import 'package:story_view/story_view.dart';
-
-class StatusModel{
-  String name;
-  String time;
-  List<StoryItem>items;
-StatusModel({this.name,this.time,this.items});
-
-}
